3f0f79f80873bfbbc1801cc8fb9ab3d70c8f160d
HuongGiang <giangch.mis@gmail.com>
2026-03-07 19:36:45 +0700

    feat: add bmi calculator app

---
+++ src/views/bmi-calculator/meta.ts
@@ -0,0 +1,5 @@
+export default {
+  name: 'BMI Calculator',
+  description: 'Công cụ tính chỉ số khối cơ thể (BMI) nhanh chóng và chuẩn xác.',
+  author: 'Nguyễn Hoàng Tùng', // Placeholder cho tên tác giả, user có thể đổi sau
+}
