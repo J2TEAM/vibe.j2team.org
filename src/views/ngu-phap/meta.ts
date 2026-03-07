f2ea8b0c444624443a169e91c5799263d71687b6
Hồ Văn Chương <231877030+germineye@users.noreply.github.com>
2026-03-07 17:29:35 +0700

    Update meta information for grammar tool
    Added Facebook link and improved description.

--- src/views/ngu-phap/meta.ts
+++ src/views/ngu-phap/meta.ts
@@ -1,5 +1,10 @@
-export default {
-  name: 'Sửa Ngữ Pháp',
-  description: 'Công cụ sửa ngữ pháp và dịch thuật',
-  author: 'germineye' 
+import type { PageMeta } from '@/types/page'
+
+const meta: PageMeta = {
+  name: 'Sửa Ngữ pháp',
+  description: 'Công cụ sửa ngữ pháp và dịch thông minh (nhất thế giới)',
+  author: 'germineye',
+  facebook: 'https://github.com/germineye',
 }
+
+export default meta
