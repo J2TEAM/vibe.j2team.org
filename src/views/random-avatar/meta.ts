eb2f961c333c0cea32ea02bdbf1a16c3a629c715
hieuhoa-dev <hieuhoa.dev.cf@gmail.com>
2026-03-07 13:52:00 +0700

    feat: add random avatar generator page
    Signed-off-by: hieuhoa-dev <hieuhoa.dev.cf@gmail.com>

---
+++ src/views/random-avatar/meta.ts
@@ -0,0 +1,9 @@
+import type { PageMeta } from '@/types/page'
+
+const meta: PageMeta = {
+  name: 'Random Avatar',
+  description: 'Random Avatar Generator',
+  author: 'Lê Hiếu Hòa'
+}
+
+export default meta
