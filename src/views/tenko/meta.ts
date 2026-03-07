e88201f9ccf0c5aedea263d2872ea18ae17d93d2
LTP <126170288+LTPPPP@users.noreply.github.com>
2026-03-07 14:13:45 +0700

    feat: tenko page

---
+++ src/views/tenko/meta.ts
@@ -0,0 +1,10 @@
+import type { PageMeta } from '@/types/page'
+
+const meta: PageMeta = {
+  name: 'Token Visualizer',
+  description: 'Công cụ trực quan hóa token và tính toán chi phí OpenAI.',
+  author: 'ltpppp',
+  facebook: 'https://www.facebook.com/groups/j2team.community',
+}
+
+export default meta
