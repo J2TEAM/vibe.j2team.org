a4ed4e1c21b5a1b6c72f1491943d32deefe1516b
behemoth-thangnt <thangnt@behemoth.vn>
2026-03-07 22:26:07 +0700

    feat: add fortune quotes feature with API integration and UI

---
+++ src/views/fortune-quotes/meta.ts
@@ -0,0 +1,11 @@
+import type { PageMeta } from '@/types/page'
+
+const meta: PageMeta = {
+  name: 'Fortune Quotes',
+  description:
+    'Trang tổng hợp những câu nói hay về cuộc sống, tình yêu, sự nghiệp và nhiều chủ đề khác.',
+  author: 'Trọng Thăng',
+  facebook: 'https://www.facebook.com/adecody.evans/',
+}
+
+export default meta
