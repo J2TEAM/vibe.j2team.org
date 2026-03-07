25087032f671c0a940e7e3f63aa7ee1b2e57cb1c
vubinh127 <vubinh120720@gmail.com>
2026-03-07 20:04:53 +0700

    feat: bingo18

---
+++ src/views/bingo18/meta.ts
@@ -0,0 +1,9 @@
+import type { PageMeta } from '@/types/page'
+
+const meta: PageMeta = {
+  name: 'Bingo 18',
+  description: 'Trò chơi dự đoán số Bingo 18 với J2-Point',
+  author: 'Bow',
+}
+
+export default meta
