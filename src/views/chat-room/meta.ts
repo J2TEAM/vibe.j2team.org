7114540d9ec095a55a6a6d6a0ebaeebd5ca2610a
t02labs <taihoang0209@gmail.com>
2026-03-08 13:07:06 +0700

    feat(views): Thêm phòng Chat Room Realtime (No-Database)

---
+++ src/views/chat-room/meta.ts
@@ -0,0 +1,10 @@
+import type { PageMeta } from '@/types/page'
+
+const meta: PageMeta = {
+  name: 'Alo-Jalo',
+  description: 'Chat trực tiếp cùng các member khác đang truy cập cái này.',
+  author: 'Tài Hoàng',
+  facebook: 'https://www.facebook.com/taihuangg191/',
+}
+
+export default meta
