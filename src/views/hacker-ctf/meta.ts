08e63add3c2095a6de51b74c011a4ea86e4aedc6
thucdang0604 <thucdang2205@gmail.com>
2026-03-08 12:30:32 +0700

    feat(hacker-ctf): init game, obfuscate random answers and fix template bugs

---
+++ src/views/hacker-ctf/meta.ts
@@ -0,0 +1,8 @@
+import type { RouteMeta } from 'vue-router'
+
+export default {
+  name: 'Hacker CTF',
+  description: 'Thử thách giải mã Front-end dành cho cộng đồng J2Team',
+  author: 'Đặng Trí Thức',
+  facebook: 'https://www.facebook.com/ThucDang64',
+} as RouteMeta
