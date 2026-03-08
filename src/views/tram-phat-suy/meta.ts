a6c3c50f890026a07c3753980a530476df4f26cd
KhanhMTK <109269313+KhanhMTK@users.noreply.github.com>
2026-03-08 22:40:59 +0700

    Update Facebook link in meta.ts

--- src/views/tram-phat-suy/meta.ts
+++ src/views/tram-phat-suy/meta.ts
@@ -2,5 +2,5 @@ export default {
   name: 'Trạm Phát Suy',
   description: 'Trạm dừng chân nghe nhạc Ngọt và vibe dành cho những tâm hồn code lỗi...',
   author: 'Kyota',
-  facebook: 'https://www.facebook.com/thay-link-cua-ban-vao-day' // Nhớ thay link Facebook thật của bạn vào đây nha
+  facebook: 'https://www.facebook.com/profile.php?id=61582395051907&locale=vi_VN'
 }
