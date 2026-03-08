84838fa582749ebd6b0420fc5b7a6f1ad462b82e
Manh Tuan <buimanhtuan2k4@gmail.com>
2026-03-08 19:18:11 +0700

    feat: update category

--- src/views/math-for-kids/meta.ts
+++ src/views/math-for-kids/meta.ts
@@ -5,6 +5,7 @@ const meta: PageMeta = {
   description: 'Mini-game giải toán dành cho mọi lứa tuổi',
   author: 'Bùi Mạnh Tuấn',
   facebook: 'https://www.facebook.com/b.manhtuan.028',
+  category: 'game',
 }
 
 export default meta
