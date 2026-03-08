0c1d7408bb2c3695c2de92bb039f6b7ddadc9674
Nolan <dongnguyenvie@gmail.com>
2026-03-08 15:18:52 +0700

    fix(sketch-wars): update author info in meta

--- src/views/sketch-wars/meta.ts
+++ src/views/sketch-wars/meta.ts
@@ -3,8 +3,8 @@ import type { PageMeta } from '@/types/page'
 const meta: PageMeta = {
   name: 'Sketch Wars',
   description: 'Vẽ đối kháng real-time 2 người chơi qua WebRTC — chiếm lãnh thổ bằng màu sắc!',
-  author: 'J2TEAM',
-  facebook: 'https://www.facebook.com/groups/j2team.community',
+  author: 'Nolan',
+  facebook: 'https://github.com/dongnguyenvie',
 }
 
 export default meta
