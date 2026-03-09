1de0f7193e56e43dbe1053cc7992d5307fadcfa0
w111user <w111user@users.noreply.github.com>
2026-03-09 16:57:43 +0700

    chore(aim-trainer): fix author handle

--- src/views/aim-trainer/meta.ts
+++ src/views/aim-trainer/meta.ts
@@ -4,7 +4,7 @@ const meta: PageMeta = {
   name: 'Aim Trainer',
   description:
     'Trò chơi luyện ngắm mục tiêu trong 30 giây, theo dõi điểm, độ chính xác và thời gian phản xạ trung bình.',
-  author: 'Đinh Trần Gia Bảo - w11user',
+  author: 'Đinh Trần Gia Bảo - w111user',
   facebook: 'https://www.facebook.com/w11user/',
   category: 'game',
 }
