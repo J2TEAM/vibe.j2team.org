8139b98d4eb7ed939dc659fe61a64135e4464cdf
ImSynZx <131290248+ImSynZx@users.noreply.github.com>
2026-03-09 14:15:20 +0700

    feat: add metadata for regex visualizer page

--- src/views/regex-visualizer/meta.ts
+++ src/views/regex-visualizer/meta.ts
@@ -3,4 +3,6 @@ export default {
   description:
     'Công cụ trực quan hóa Regex: giải thích biểu thức, highlight match, mô phỏng regex engine và phân tích hiệu năng.',
   author: 'SynZx',
+  facebook: 'https://www.facebook.com/imsynzx',
+  category: 'tool',
 }
