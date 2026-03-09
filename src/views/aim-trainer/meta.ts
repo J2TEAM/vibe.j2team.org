f9e5f7664252ec81fffd5f80bd6085bb41c1f9b2
w111user <w111user@users.noreply.github.com>
2026-03-09 16:33:31 +0700

    feat(aim-trainer): add configurable aim trainer with scoreboard

---
+++ src/views/aim-trainer/meta.ts
@@ -0,0 +1,12 @@
+import type { PageMeta } from '@/types/page'
+
+const meta: PageMeta = {
+  name: 'Aim Trainer',
+  description:
+    'Trò chơi luyện ngắm mục tiêu trong 30 giây, theo dõi điểm, độ chính xác và thời gian phản xạ trung bình.',
+  author: 'Trần Hoàng Nhật - THNDEV05',
+  facebook: 'https://www.facebook.com/thn.ishungry/',
+  category: 'game',
+}
+
+export default meta
