0931195b3c2dc65ce0ff20b6ea6110a3338f452f
w111user <w111user@users.noreply.github.com>
2026-03-09 16:56:53 +0700

    chore(aim-trainer): update author metadata

--- src/views/aim-trainer/meta.ts
+++ src/views/aim-trainer/meta.ts
@@ -4,8 +4,8 @@ const meta: PageMeta = {
   name: 'Aim Trainer',
   description:
     'Trò chơi luyện ngắm mục tiêu trong 30 giây, theo dõi điểm, độ chính xác và thời gian phản xạ trung bình.',
-  author: 'Trần Hoàng Nhật - THNDEV05',
-  facebook: 'https://www.facebook.com/thn.ishungry/',
+  author: 'Đinh Trần Gia Bảo - w11user',
+  facebook: 'https://www.facebook.com/w11user/',
   category: 'game',
 }
 
