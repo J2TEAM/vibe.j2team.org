081c7086e029f7c033eafbe8b1cb59d8edb25caa
lavadevv <lamubboy@gmail.com>
2026-03-09 15:40:29 +0700

    chore: update commit-generator meta to match author and facebook

--- src/views/commit-generator/meta.ts
+++ src/views/commit-generator/meta.ts
@@ -4,7 +4,9 @@ const meta: PageMeta = {
   name: 'Commit Message Generator',
   description:
     'Biến mô tả việc vừa làm thành commit message theo nhiều style cực vibe: Lục bát, Gen Z, Passive-aggressive...',
-  author: 'Antigravity x J2TEAM',
+  author: 'd4phuclam',
+  facebook: 'https://www.facebook.com/lamdo299',
+  category: 'tool',
 }
 
 export default meta
