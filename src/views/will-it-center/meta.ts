c5b1be50d9b038a5cb7bc9588579d0ea6b1fa2e7
light <shinigamii.hikaru@gmail.com>
2026-03-09 18:06:51 +0700

    feat: add category

--- src/views/will-it-center/meta.ts
+++ src/views/will-it-center/meta.ts
@@ -3,7 +3,7 @@ import type { PageMeta } from '@/types/page'
 const meta: PageMeta = {
     name: 'Will It Center?',
     description: 'Thử thách CSS: Liệu bạn có thể canh giữa div này không?',
-    // category: 'game',
+    category: 'learn',
     author: 'Light',
 }
 
