81fe28e5f24b2eb677a5d6a6c8ae0bf272ebfd60
Admin <truongdinhanh27@gmail.com>
2026-03-09 22:22:25 +0700

    feat: add nga-re-cuoc-doi view

---
+++ src/views/nga-re-cuoc-doi/meta.ts
@@ -0,0 +1,8 @@
+import type { PageMeta } from '@/types/page'
+
+const meta: PageMeta = {
+  name: 'Ngã Rẽ Cuộc Đời',
+  description: 'Mỗi lựa chọn, một định mệnh. Khám phá các ngả rẽ khác nhau trong cuộc đời bạn!',
+  author: 'Đình Anh',
+}
+export default meta
