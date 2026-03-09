16458497d9d6afdcca8044f1f136b6cecb92d21b
Trieu Nguyen Thanh <trieukon1011@gmail.com>
2026-03-09 11:03:53 +0700

    fix: update meta file

--- src/views/word-chain-king/meta.ts
+++ src/views/word-chain-king/meta.ts
@@ -4,6 +4,7 @@ const meta: PageMeta = {
   name: 'Vua nối từ',
   description: 'Nối từ để giải tỏa áp lực coding! Nối càng nhanh, vibe càng cao.',
   author: 'Konnn04',
+  facebook: 'https://www.facebook.com/konnn.1011',
   category: 'game',
 }
 
