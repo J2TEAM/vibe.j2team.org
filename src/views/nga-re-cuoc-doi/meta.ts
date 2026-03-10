2800d19e70af567fbb502f6f32c6cbede6e6c49e
Admin <truongdinhanh27@gmail.com>
2026-03-10 11:17:24 +0700

    chore: merge upstream main and fix meta category

--- src/views/nga-re-cuoc-doi/meta.ts
+++ src/views/nga-re-cuoc-doi/meta.ts
@@ -4,5 +4,6 @@ const meta: PageMeta = {
   name: 'Ngã Rẽ Cuộc Đời',
   description: 'Mỗi lựa chọn, một định mệnh. Khám phá các ngả rẽ khác nhau trong cuộc đời bạn!',
   author: 'Đình Anh',
+  category: 'game',
 }
 export default meta
