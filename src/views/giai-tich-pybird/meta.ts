998aa2b4fc654211eea2c8b967be800a59caa487
Lizi <86664792+whooslizi@users.noreply.github.com>
2026-03-10 19:39:36 +0700

    fix: removed github

--- src/views/giai-tich-pybird/meta.ts
+++ src/views/giai-tich-pybird/meta.ts
@@ -3,8 +3,7 @@ import type { PageMeta } from '@/types/page'
 const meta: PageMeta = {
   name: 'Giải tích PyBird',
   description: 'Giải tích but in Flappy Bird.',
-  author: 'whooslizi',
-  github: 'https://github.com/whooslizi',
+  author: 'WhoosLizi',
   category: 'game',
 }
 
