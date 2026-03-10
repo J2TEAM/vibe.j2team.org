1c450f21440b7b398c4f85fbe1acc74d8a9a2c66
Lizi <86664792+whooslizi@users.noreply.github.com>
2026-03-10 19:31:31 +0700

    feat. added metadata for the page

---
+++ src/views/giai-tich-pybird/meta.ts
@@ -0,0 +1,11 @@
+import type { PageMeta } from '@/types/page'
+
+const meta: PageMeta = {
+  name: 'Giải tích PyBird',
+  description: 'Giải tích but in Flappy Bird.',
+  author: 'whooslizi',
+  github: 'https://github.com/whooslizi',
+  category: 'games',
+}
+
+export default meta
