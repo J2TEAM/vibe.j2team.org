c6df7bedb90f72f171b90f55948e90c78c6a0d42
Mai Hoàng Quốc Bảo <mhqb365@gmail.com>
2026-03-11 12:13:48 +0700

    feat(dice): update meta information for dice game

--- src/views/dice/meta.ts
+++ src/views/dice/meta.ts
@@ -4,7 +4,7 @@ const meta: PageMeta = {
   name: 'DICE',
   description: 'Trò chơi xúc xắc minh bạch dựa trên thuật toán mã hóa',
   author: 'mhqb365',
-  facebook: 'https://mhqb365.com',
+  facebook: 'https://mhqb365.com/',
   category: 'game',
 }
 
