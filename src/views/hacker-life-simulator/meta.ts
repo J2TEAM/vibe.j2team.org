2a49fa52a6d4c51c64b69620a93a4dec05a5de68
gitprovpn <seedance.v2.0@gmail.com>
2026-03-12 13:50:30 +0700

    Update meta.ts

--- src/views/hacker-life-simulator/meta.ts
+++ src/views/hacker-life-simulator/meta.ts
@@ -2,10 +2,12 @@ import type { PageMeta } from '@/types/page'
 
 const meta: PageMeta = {
   name: 'Hacker Life Simulator',
-  description: 'Spam bàn phím để hack theo phong cách Hollywood với split terminal, popup ACCESS GRANTED và cyberpunk sound.',
+  description:
+    'Spam bàn phím để hack theo phong cách Hollywood với split terminal, fake intel monitor và popup ACCESS GRANTED.',
   author: 'Thanhlm',
   facebook: 'https://www.facebook.com/f4loveyou',
   category: 'fun',
+  showToolbar: false,
 }
 
 export default meta
