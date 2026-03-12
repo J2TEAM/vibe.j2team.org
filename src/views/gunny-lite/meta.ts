53f9a481e893022c7ae29e4635cef6bcfb12fd72
Nguyễn Lê Anh Minh <anhminh2122000@gmail.com>
2026-03-13 01:53:33 +0800

    feat(gunny-lite): add gunny lite game

---
+++ src/views/gunny-lite/meta.ts
@@ -0,0 +1,10 @@
+import type { PageMeta } from '@/types/page'
+
+const meta: PageMeta = {
+  name: 'Gunny Lite',
+  description: 'Game bắn súng tọa độ đỉnh cao',
+  author: 'nabatti99',
+  category: 'game',
+}
+
+export default meta
