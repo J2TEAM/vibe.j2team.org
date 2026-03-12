0605561fea41beb1a0720591ba779216d123e4e8
Nguyễn Lê Anh Minh <anhminh2122000@gmail.com>
2026-03-13 01:57:02 +0800

    chore: update meta file

--- src/views/gunny-lite/meta.ts
+++ src/views/gunny-lite/meta.ts
@@ -5,6 +5,7 @@ const meta: PageMeta = {
   description: 'Game bắn súng tọa độ đỉnh cao',
   author: 'nabatti99',
   category: 'game',
+  facebook: 'https://www.facebook.com/nguyenanhminh02',
 }
 
 export default meta
