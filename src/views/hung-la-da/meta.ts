7d73196df587dc723f9052408259d7da80855dc4
TranVi <tranvanvi2855@gmail.com>
2026-03-14 19:32:44 +0700

    feat:  add hung-la-da

---
+++ src/views/hung-la-da/meta.ts
@@ -0,0 +1,11 @@
+import type { PageMeta } from '@/types/page'
+
+const meta: PageMeta = {
+  name: 'Hứng Lá Đa',
+  description: 'Hứng Lá Đa - Tích công đức.',
+  author: 'Trần Văn Vĩ',
+  facebook: 'https://github.com/hubgamezz',
+  category: 'game',
+}
+
+export default meta
