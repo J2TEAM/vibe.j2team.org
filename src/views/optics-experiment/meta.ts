d908ac860a03d371fa88fe006d8db623a7802609
Tạ Hữu Cường <dragonsvip090@gmail.com>
2026-03-14 15:34:01 +0700

    feat: add optics experiment page

---
+++ src/views/optics-experiment/meta.ts
@@ -0,0 +1,10 @@
+import type { PageMeta } from '@/types/page'
+
+const meta: PageMeta = {
+  name: 'Thí nghiệm quang học',
+  description: 'Mô phỏng các thí nghiệm với ánh sáng',
+  author: 'iTCuong',
+  category: 'learn',
+}
+
+export default meta
