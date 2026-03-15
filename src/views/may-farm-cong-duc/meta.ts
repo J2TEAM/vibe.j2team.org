f9b71dd5dacc55781d23934dc1d95d8e3967165f
khanh <khanhdacodau2007@gmail.com>
2026-03-15 12:06:11 +0700

    feat: them trang may farm cong duc

---
+++ src/views/may-farm-cong-duc/meta.ts
@@ -0,0 +1,11 @@
+import type { PageMeta } from '@/types/page'
+
+const meta: PageMeta = {
+  name: 'Máy farm công đức',
+  description: 'Tích đức nhận tài lộc',
+  author: 'Bùi Khanh',
+  facebook: 'https://web.facebook.com/buikhanh0411',
+  category: 'fun',
+}
+
+export default meta
