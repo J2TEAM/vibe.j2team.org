9c60d1d6447fcb767962fc823fdde48c02063095
KingChen711 <kingchenobama711@gmail.com>
2026-03-16 23:07:49 +0700

    chore: add author facebook link in meta.ts

--- src/views/digits-packing-puzzle/meta.ts
+++ src/views/digits-packing-puzzle/meta.ts
@@ -4,6 +4,7 @@ const meta: PageMeta = {
   name: 'Digits Packing Puzzle',
   description: 'Xếp hình số từ 0-9 trên bảng 5x4 với các đoạn thẳng 7-segment display',
   author: 'King Chen',
+  facebook: 'https://www.facebook.com/king.chen.739978',
   category: 'game',
 }
 
