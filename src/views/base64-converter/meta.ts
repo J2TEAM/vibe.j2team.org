c0cdb529a1571ddc284c9a530e2f67e6605026e7
vubinh127 <vubinh120720@gmail.com>
2026-03-17 22:05:24 +0700

    fix: fix add category in meta.ts

--- src/views/base64-converter/meta.ts
+++ src/views/base64-converter/meta.ts
@@ -4,7 +4,7 @@ const meta: PageMeta = {
   name: 'Base64 Converter',
   description: 'Công cụ chuyển đổi ảnh sang base64',
   author: 'Bow',
-  category: 'Tools',
+  category: 'tool',
 }
 
 export default meta
