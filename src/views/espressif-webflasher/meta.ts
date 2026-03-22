85a1a07f2aad5978b7b0606b9ae87ed21c4f79fe
Đinh Trung Kiên <110721139+ryzen30xx@users.noreply.github.com>
2026-03-23 01:08:01 +0700

    feat(espressif-webflasher): add page meta

---
+++ src/views/espressif-webflasher/meta.ts
@@ -0,0 +1,12 @@
+import type { PageMeta } from '@/types/page'
+
+const meta: PageMeta = {
+  name: 'Espressif Web Flasher',
+  description:
+    'Công cụ code và nạp firmware trực tiếp từ trình duyệt cho các bo mạch Espressif (ESP8266, ESP32, ...)',
+  author: 'Đinh Trung Kiên',
+  facebook: 'https://www.facebook.com/Kprivatespace',
+  category: 'tool',
+}
+
+export default meta
