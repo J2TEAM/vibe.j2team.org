344f8941ba347cf964a2a29cb1d2400286c7ad39
MTDES23 <thachminhtri23@gmail.com>
2026-03-27 23:41:33 +0700

    feat: add J2 Dinosaur game with crimson red theme

---
+++ src/views/dino/meta.ts
@@ -0,0 +1,11 @@
+import type { PageMeta } from '@/types/page'
+
+const meta: PageMeta = {
+  name: 'J2 Dinosaur',
+  description: 'Trò chơi khủng long huyền thoại.',
+  author: 'mtdes23',
+  facebook: 'https://www.facebook.com/mtdes233/',
+  category: 'game',
+}
+
+export default meta
